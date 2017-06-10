aec49f8c60d95cc0a66212c9765782c874e6f79e
Saim Farid <saimfd00@gmail.com>
2017-06-10 11:12:09 +0500

    Added /users routes and token method

--- config/config.js
+++ config/config.js
@@ -1,5 +1,4 @@
 var env = process.env.NODE_ENV || 'development';
-console.log(env);
 if(env === 'development'){
   process.env.PORT = 3000;
   process.env.MONGODB_URI = 'mongodb://localhost/TodoApp'
