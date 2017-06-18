e041e336a6008ff278eeeb5304c17a012cd18b9e
Saim Farid <saimfd00@gmail.com>
2017-06-18 18:35:33 +0500

    Add test for DELETE /users/me/token

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -309,7 +309,31 @@ describe('POST /users/login', () => {
             return done(err)
           }
           done()
-
         })
     });
+});
+
+describe('DELETE /users/me/token', () => {
+  it('should delete auth token on logout', (done) => {
+    request(app)
+      .delete('/users/me/token')
+      .set('x-auth', users[0].tokens[0].token)
+      .expect(200)
+      .end((err, res) => {
+        if(err){
+          return done(err)
+        }
+
+        User.findById(users[0]._id).then((user) => {
+          expect(user.tokens.length).toBe(0);
+          done();
+        }).catch((e) => done(e));
+
+      })
+  });
+
+  // it('description', (done) => {
+  //
+  // });
+
 });
